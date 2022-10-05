a6172a51c0ee153e0c7b1ce1341e762ee92d1ecd
GeniusDP <zaranikbz@gmail.com>
2022-10-05 23:40:58 +0300

    e-mail dto and non-implemented service

---
+++ server/services/email_service/src/main/java/com/kpi/zaranik/third_service/dto/request/EMailDetails.java
@@ -0,0 +1,28 @@
+package com.kpi.zaranik.third_service.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class EMailDetails {
+
+    @Email
+    @NotNull
+    private String emailTo;
+
+    @NotNull(message = "caption of message must not be omitted")
+    @Size(min = 5, message = "caption must not be less than 5 symbols")
+    private String caption;
+
+    @NotNull(message = "caption of message must not be omitted")
+    private String messageBody;
+
+}
