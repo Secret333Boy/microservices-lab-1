fe5b855a948c9c17faab6238efd8e4372301c40d
GeniusDP <zaranikbz@gmail.com>
2022-10-05 23:40:49 +0300

    e-mail controller

---
+++ server/services/email_service/src/main/java/com/kpi/zaranik/third_service/controllers/EMailController.java
@@ -0,0 +1,25 @@
+package com.kpi.zaranik.third_service.controllers;
+
+import com.kpi.zaranik.third_service.dto.request.EMailDetails;
+import com.kpi.zaranik.third_service.services.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/email")
+@RequiredArgsConstructor
+public class EMailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/send")
+    public String sendEMail(@RequestBody @Valid EMailDetails details){
+        return mailService.sendEMail(details);
+    }
+
+}
