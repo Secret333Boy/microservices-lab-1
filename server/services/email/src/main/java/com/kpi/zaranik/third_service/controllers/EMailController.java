8fcf7a149d3dbaaee9bff00fcffb569fe4deaca9
GeniusDP <zaranikbz@gmail.com>
2022-10-30 21:22:47 +0200

    added context path => removed RequestMapping

--- server/services/email/src/main/java/com/kpi/zaranik/third_service/controllers/EMailController.java
+++ server/services/email/src/main/java/com/kpi/zaranik/third_service/controllers/EMailController.java
@@ -7,7 +7,6 @@ import com.kpi.zaranik.third_service.services.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
