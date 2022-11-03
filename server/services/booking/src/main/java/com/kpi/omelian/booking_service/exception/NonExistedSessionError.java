7440b5ce1f74102455840c09c0b70267fdec7abd
GeniusDP <zaranikbz@gmail.com>
2022-11-03 14:07:26 +0200

    replaced extends Exception with extends RuntimeException for custom exceptions: more appropriate way

--- server/services/booking/src/main/java/com/kpi/omelian/booking_service/exception/NonExistedSessionError.java
+++ server/services/booking/src/main/java/com/kpi/omelian/booking_service/exception/NonExistedSessionError.java
@@ -1,6 +1,6 @@
 package com.kpi.omelian.booking_service.exception;
 
-public class NonExistedSessionError extends Exception {
+public class NonExistedSessionError extends RuntimeException {
 
     public static final String ERROR_MESSAGE = "Sorry, session doesn't exist, so it can't be deleted.";
 
--- server/services/booking/src/main/java/com/kpi/omelian/booking_service/exception/NonExistedTicketError.java
+++ server/services/booking/src/main/java/com/kpi/omelian/booking_service/exception/NonExistedTicketError.java
@@ -1,6 +1,6 @@
 package com.kpi.omelian.booking_service.exception;
 
-public class NonExistedTicketError extends Exception {
+public class NonExistedTicketError extends RuntimeException {
 
     public static final String ERROR_MESSAGE = "Sorry, ticket can't be deleted.";
 
