14d7a3691020b38441386ade37114c91407faec5
GeniusDP <zaranikbz@gmail.com>
2022-11-03 14:51:29 +0200

    added some comments for SecuredEndpoint.java

--- server/services/booking/src/main/java/com/kpi/omelian/booking_service/aspect/SecuredEndpoint.java
+++ server/services/booking/src/main/java/com/kpi/omelian/booking_service/aspect/SecuredEndpoint.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/*
+* Put it on a controller-level method endpoint
+* and access verification logic will be automatically
+* applied for it
+* */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SecuredEndpoint {
